f27030eb90c43cc49f7992f05e9af408b524ab35
Nurunnahar <nurunnahar9081@gmail.com>
2024-05-03 14:42:34 +0600

    analog clock with javascript

--- src/main.js
+++ src/main.js
@@ -1,20 +1,18 @@
-const sec = document.querySelector('.second');
+const sec = document.querySelector(".second");
 const min = document.querySelector(".minute");
-const hour = document.querySelector('.hour');
-
-
+const hour = document.querySelector(".hour");
 
 setInterval(() => {
-    let time = new Date();
-    let current_sec = time.getSeconds();
-    let current_min = time.getMinutes();
-    let current_hour = time.getHours();
+  let time = new Date();
+  let current_sec = time.getSeconds();
+  let current_min = time.getMinutes();
+  let current_hour = time.getHours();
 
-    sec.style.transform = `rotate(${katakuta(60, current_sec)}deg)`;
-    min.style.transform = `rotate(${katakuta(60, current_min)}deg)`;
-    hour.style.transform = `rotate(${katakuta(12, current_hour)}deg)`;
+  sec.style.transform = `rotate(${katakuta(60, current_sec)}deg)`;
+  min.style.transform = `rotate(${katakuta(60, current_min)}deg)`;
+  hour.style.transform = `rotate(${katakuta(12, current_hour)}deg)`;
 }, 1000);
 
 function katakuta(time, current) {
-    return (360 * current) / time;
+  return (360 * current) / time;
 }
